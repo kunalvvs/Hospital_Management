3a825beca1f05455b8cdcaed8bc887e9e4e9b0e8
Kunal <97425818+kunalvvs@users.noreply.github.com>
2024-03-13 19:56:01 +0530

    Complete

---
+++ Hospital_Management/Doctor.java
@@ -0,0 +1,39 @@
+package Hospital_Management;
+
+import java.util.Scanner;
+
+public class Doctor extends Entry{
+    void met()
+    {
+        System.out.println("Which doctor do you want \nPhysician\nRadiologist\nNeurosurgen");
+        Scanner sc = new Scanner(System.in);
+        String ch = sc.nextLine();
+        switch(ch)
+        {
+            case "Physician":
+            System.out.println("Physcians: Dr.Ajay Sharma\nDr.Rahul Rajput ");
+            reg();
+            break;
+            case "physician":
+            System.out.println("Physcians: Dr.Ajay Sharma\nDr.Rahul Rajput ");
+            reg();
+            break;
+            case "Radiologist":
+            System.out.println("Dr.Titu Verma\nDr.Shubh Agarwal");
+            reg();
+            break;
+            case "Neurosurgen":
+            System.out.println("Dr.Amir Usain\nDr.B.K Gupta");
+            reg();
+            break;
+
+            default:
+            System.out.println("Oops! you have choose incorrect name\n Choose Doctor to checkup");
+            met();
+        }
+        
+        
+        
+    }
+}
+
---
+++ Hospital_Management/Entry.java
@@ -0,0 +1,78 @@
+package Hospital_Management;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Entry implements enter {
+
+   public void reg()
+   
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Pay fee 200rs of form: ");
+      //  int as;
+      try{
+        int fee = sc.nextInt();
+      sc.nextLine();
+       if(fee==200){
+      System.out.println("Fill this form: ");
+
+      System.out.print("Patient Name: ");
+      String patientName = sc.nextLine();
+      // sc.nextLine();
+      
+      System.out.print("Age: ");
+      int patientAge = sc.nextInt();
+      sc.nextLine();
+        
+      
+      System.out.print("Phone no.: ");
+      long phoneNumber = sc.nextLong();
+      sc.close();
+       
+        try{
+        for(int i=0;i<15;i++)
+          {
+          System.out.print(".");
+        Thread.sleep(500);
+          }       
+        }
+        catch(Exception e){}
+        // System.out.println(phoneNumber);
+         System.out.print("Verifing your details..");
+        try{
+          for(int i=0;i<15;i++)
+          {
+          System.out.print(".");
+        Thread.sleep(500);
+          }
+        }
+        catch(Exception e)
+        {
+          System.out.println("");
+        }
+
+        System.out.println("\n---------------Verified your details-----------------");
+        Random r = new Random();
+        int randNum = r.nextInt(50);
+        int roomNum =r.nextInt(12);
+        System.out.println("Generated Token \nYour Token no. is "+randNum+"\nRoom no. "+roomNum);
+        
+        }
+         else if(fee>200 || fee<200)
+         {
+          System.out.println("Please pay 200rs only ");
+          reg();
+         }
+           else 
+       {
+        System.out.println("pay form fee..");
+        reg();
+       }
+      }
+      catch(Exception e)
+      {
+        System.out.println("Don't support alphabets");
+        reg();
+      }
+    }
+}
